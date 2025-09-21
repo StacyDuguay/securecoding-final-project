03f0d482850e6431a121094ae70946e5654da9f4
Stacy Duguay <sduguay@rrc.ca>
2025-09-21 14:44:04 -0500

    Feat: Added routes for getEmployeeByDepartment and getEmployeeByBranch.

--- src/api/v1/routes/employeeRoutes.ts
+++ src/api/v1/routes/employeeRoutes.ts
@@ -4,8 +4,10 @@ import * as employeeController from "../controllers/employeeController";
 const router: Router = Router();
 
 router.get("/", employeeController.getAllEmployees);
-router.post("/", employeeController.createEmployee);
 router.get("/:id", employeeController.getEmployeeById);
+router.get("/branch/:branchId", employeeController.getEmployeesByBranch);
+router.get("/department/:department", employeeController.getEmployeesByDepartment);
+router.post("/", employeeController.createEmployee);
 router.put("/:id", employeeController.updateEmployee);
 router.delete("/:id", employeeController.deleteEmployee);
 
