3d68ee8a1d7f7fd304cf617b7d9d0ce5e95b6c84
Stacy Duguay <sduguay@rrc.ca>
2025-10-01 09:10:12 -0500

    Feat: Initial commit for assignment three created the updated respository structure.

--- jest.config.js
+++ jest.config.js
@@ -8,3 +8,8 @@ module.exports = {
         "!src/types/**/*.ts", 
     ],
 };
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
+};
