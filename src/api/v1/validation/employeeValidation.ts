37b9f0515733bdb966a41fcf885f454e90c1e783
Stacy Duguay <sduguay@rrc.ca>
2025-10-02 11:22:15 -0500

    FIX: Added validation for evrything within the employee model and more rigid validation for phone number and email.

--- src/api/v1/validation/employeeValidation.ts
+++ src/api/v1/validation/employeeValidation.ts
@@ -12,17 +12,36 @@ export const employeeSchemas = {
                 "any.required": "Position is required",
                 "string.empty": "Position cannot be empty"
             }),
-            email : Joi.string().required().messages({
+            department: Joi.string().required().messages({
+                "any.required": "Department is required",
+                "string.empty": "Department cannot be empty",
+            }),
+            email : Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'ca', 'gov', 'net']}})
+            .required()
+            .messages({
                 "any.required": "Email is required",
+                "string.email": "Email must be valid",
                 "string.empty": "Email cannot be empty"
             }),
-            branchid: Joi.string().required().messages({
-                "any.required": "Branchid is required",
-                "string.empty": "Branchid cannot be empty"
+            phone: Joi.string().pattern(/^[0-9\-+() ]{7,20}$/).required()
+            .messages({
+                "any.required": "Phone number is required",
+                "string.empty": "Phone number cannot be empty",
+                "string.pattern.base" : "Phone number must be valid"
+            }),
+            branchId: Joi.number().integer().required().messages({
+                "any.required": "BranchId is required",
             }),
         }),
     },
     update: {
+        params: Joi.object({
+            id: Joi.string().required().messages({
+                "any.required": "Item ID is required",
+                "string.empty": "Item ID cannot be empty",
+            }),
+        }),
         body: Joi.object({
             name: Joi.string().required().messages({
                 "string.empty": "Name cannot be empty"
@@ -30,11 +49,19 @@ export const employeeSchemas = {
             position: Joi.string().required().messages({
                 "string.empty": "Position cannot be empty"
             }),
+            department: Joi.string().messages({
+                "string.empty": "Department cannot be empty",
+            }),
             email : Joi.string().required().messages({
+                "string.email": "Email must be valid",
                 "string.empty": "Email cannot be empty"
             }),
-            branchid: Joi.string().required().messages({
-                "string.empty": "Branchid cannot be empty"
+            phone: Joi.string().pattern(/^[0-9\-+() ]{7,20}$/)
+            .messages({
+                "string.pattern.base": "Phone must be a valid number",
+            }),
+            branchId: Joi.string().required().messages({
+                "string.empty": "BranchId cannot be empty"
             }),
         }),
     }, 
